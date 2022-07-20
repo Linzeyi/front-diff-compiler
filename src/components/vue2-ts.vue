efc6ee7d819c4379d4664bce74c95e2da4ad14c7
chenmq <1075124372@qq.com>
2022-07-20 15:37:24 +0800

    新增vue2-ts.vue

---
+++ src/components/vue2-ts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <normal-cash-coupon v-if="payScene === 'common'"></normal-cash-coupon>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { PayCommon } from '../types/payCommon';
+import usePayCashCoupon from '../hooks/coupon/usePayCashCoupon';
+import NormalCashCoupon from '../payscene/normal/NormalCashCoupon.vue';
+
+export default defineComponent({
+  name: 'PayCashCoupon',
+  setup(props: PayCommon.Data, ctx: PayCommon.SetupContext) {
+    const payCashCoupon = usePayCashCoupon(ctx);
+    return {
+      ...payCashCoupon
+    };
+  },
+  components: {
+    NormalCashCoupon
+  }
+});
+</script>
