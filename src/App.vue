c31747b2b17764b899e840d44914b03c06d09d50
linzeyi <zeyiilin@gmail.com>
2022-07-21 21:38:51 +0800

    build 清空src目录和日志，重造demo

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,7 @@
 </template>
 
 <script>
-  /**
-   * @vue-prop {Number} initialCounter - Initial counter's value
-   * @vue-prop {Number} [step=1] - Step
-   * @vue-data {Number} counter - Current counter's value
-   * @vue-computed {String} message
-   * @vue-event {Number} increment - Emit counter's value after increment
-   * @vue-event {Number} decrement - Emit counter's value after decrement
-   */
   export default {
-    props: {
-      initialCounter: {
-        type: Number,
-        required: true,
-      },
-      step: {
-        type: Number,
-        default: 1,
-      },
-    },
-    data () {
-      return {
-        counter: 0,
-      }
-    },
-    computed: {
-      message() {
-        return `Current value is ${this.counter}`;
-      }
-    },
-    methods: {
-      
-      increment() {
-        this.counter += 1;
-        this.$emit('increment', this.counter);
-      },
-      decrement() {
-        this.counter -= 1;
-        this.$emit('decrement', this.counter);
-      }
-    }
+    name: 'app'
   }
 </script>
--- src/utils/test.js
+++
@@ -1,1 +0,0 @@
-const testa = 123
